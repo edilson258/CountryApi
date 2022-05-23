a70e045a4406b0f59d5e2740746ac7a651191d22
edilson258 <developeredilson@hotmail.com>
2022-05-23 17:22:51 +0200

    added more fields to the model

--- src/api/models.py
+++ src/api/models.py
@@ -5,12 +5,15 @@ from uuid import uuid4
 
 class Country(models.Model):
     
-    _id = models.UUIDField(unique=True, editable=False, default=uuid4())
+    _uuid = models.UUIDField(unique=True, editable=False, default=uuid4())
     name = models.CharField(max_length=100, null=False, blank=False)
     official_name = models.CharField(max_length=100, null=False, blank=False)
-    ISO_code = models.CharField(max_length=3, null=False, blank=False)
+    alpha_2 = models.CharField(max_length=2, null=False, blank=False)
+    alpha_3 = models.CharField(max_length=3, null=False, blank=False)
+    numeric_code = models.IntegerField(max_length=3, null=False, blank=False)
+    ISO_code = models.CharField(max_length=20, null=False, blank=False)
     capital = models.CharField(max_length=100, null=False, blank=False)
-    language = models.CharField(max_length=100, null=False, blank=False)
+    official_language = models.CharField(max_length=100, null=False, blank=False)
     phone_code = models.CharField(max_length=10, null=False, blank=False)
     currency_name = models.CharField(max_length=50, null=False, blank=False)
     currency_code = models.CharField(max_length=10, null=False, blank=False)
